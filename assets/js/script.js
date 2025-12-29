926ae369cd9b7a534aaa32be2fb3273ed23866ce
Brenno Thiago <167652253+Brennoth@users.noreply.github.com>
2025-12-28 23:46:29 -0300

    Calculadora

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+(function () {
+    const inputText = document.querySelector("#inputText")
+    const calculadora = {
+        start() {
+            document.addEventListener("click", function (click) {
+                if (click.target.classList.contains("btn")) {
+                    const btn = click.target.innerText
+                    inputText.value += btn
+                }
+            })
+            this.clear()
+            this.resultado()
+            this.del()
+        },
+
+        clear() {
+            document.addEventListener("click", function (click) {
+                if (click.target.classList.contains("clear")) {
+                    inputText.value = ""
+                }
+            })
+        },
+
+        del() {
+            document.addEventListener("click", function (click) {
+                if (click.target.classList.contains("del")) {
+                    inputText.value = inputText.value.slice(0, -1)
+                }
+            })
+        },
+
+        resultado() {
+            document.addEventListener("click", function (click) {
+                if (click.target.classList.contains("res")) {
+                    if (inputText.value === "") {
+                        return
+                    } else {
+
+                        const expressaoValida = /^[0-9+\-*/.]+$/
+                        if (expressaoValida) {
+
+                            const valor = inputText.value
+                            const resultado = eval(valor)
+
+                            //Colocando a frase carregando e a nota 10
+                            const time = setTimeout(function () {
+                                if (resultado === 10) {
+                                    inputText.value = "vc e nota 10 arthur"
+                                } else {
+                                    inputText.value = resultado
+                                }
+                            }, 2000)
+
+
+
+                            inputText.value = "Carregando..."
+                        }
+                    }
+
+
+
+                }
+            })
+        }
+
+
+    }
+    calculadora.start()
+
+    // console.log(inputText)
+
+})()
